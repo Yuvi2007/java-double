e8fa75f9b73def9cc58a5c369bc39ad4bbe54d48
Yuvi2007 <144378404+Yuvi2007@users.noreply.github.com>
2023-09-15 13:06:56 -0400

    Add files via upload

---
+++ Main-3.java
@@ -0,0 +1,11 @@
+//program to add two numbers
+class Main {
+  public static void main(String[] args) {
+    double y;y = 7.5 ;
+    double x;x = 5 ;
+    System.out.println(x*y);
+    System.out.println(x/y);
+    System.out.println(x+y);
+    System.out.println(x-y);
+  }
+}
